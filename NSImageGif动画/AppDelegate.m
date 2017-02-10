903eeb112fbf88c00b77aa9921ccf1eb15263e20
zhengwei931102 <admin@admindeMac-mini.local>
2017-02-10 08:38:30 +0800

    NSImageGif

--- NSImageGif动画/AppDelegate.m
+++ NSImageGif动画/AppDelegate.m
@@ -11,12 +11,20 @@
 @interface AppDelegate ()
 
 @property (weak) IBOutlet NSWindow *window;
+@property (weak) IBOutlet NSImageView *GifImageView;
 @end
 
 @implementation AppDelegate
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
-    // Insert code here to initialize your application
+    
+    //动画(NSImageView 相当于 UIImageView 的动画，差别：直接用GIF)
+        _GifImageView.imageScaling = NSImageScaleAxesIndependently;
+        [_GifImageView setAnimates:YES];
+        [_GifImageView setImage:[NSImage imageNamed:@"loading.gif"]];
+        _GifImageView.canDrawSubviewsIntoLayer = YES;
+    
+    
 }
 
 - (void)applicationWillTerminate:(NSNotification *)aNotification {
